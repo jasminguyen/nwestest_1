b02bbe5c6d8a9fb9605c91cfa15561137dc64eab
jasmin nguyen <jasmin.nguyen@bulme.at>
2021-12-22 09:04:41 +0100

    1.try

--- serverlein.py
+++ serverlein.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 
+import socket
+
+HOST = ''                 
+PORT = 8080           
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    while True: 
+        conn, addr = s.accept()
+        with conn:
+            print('GET / HTTP/1.1\r\nHost: localhost\r\n\r\n', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                outdata = 'HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Hi BULME</h1></body></html>'.encode()
+                print(outdata)
+                conn.sendall(outdata)
+                close()
